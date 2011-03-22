c1d8365f5a8ba0b9607b99ff2efdd651a3b4606b
Kevin Scroggins <nitro404@gmail.com>
2011-03-22 18:44:52 +0000

    Added ByteStream to planner
    git-svn-id: file:///e/SVN/tsep@11 9e8b06f4-fe92-4974-b243-337f9da7b896

--- MasterPlanner/src/shared/ConsoleEntry.java
+++ MasterPlanner/src/shared/ConsoleEntry.java
@@ -4,26 +4,27 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class ConsoleEntry {
+	
     private Calendar m_time;
     private String m_text;
 
-    final private static SimpleDateFormat dateFormat = new SimpleDateFormat(
-	    "yyyy-M-d hh:mm:ss a");
+    final private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-M-d hh:mm:ss a");
 
     public ConsoleEntry(String text) {
-	m_time = Calendar.getInstance();
-	m_text = text;
+		m_time = Calendar.getInstance();
+		m_text = text;
     }
 
     public Calendar getTime() {
-	return m_time;
+    	return m_time;
     }
 
     public String getTimeString() {
-	return dateFormat.format(m_time.getTime());
+    	return dateFormat.format(m_time.getTime());
     }
 
     public String getText() {
-	return m_text;
+    	return m_text;
     }
+    
 }
